a68210dfcd26876748908b9d371b972905ba6ef1
Narezzzs <nareswara353@gmail.com>
2026-09-04 22:29:57 +0700

    feat(application): add DI extension for MediatR and FluentValidation registration

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
