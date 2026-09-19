bcbc94551de068293a6e479a27cdb5bc63eba83a
Narezzzs <nareswara353@gmail.com>
2026-09-19 20:22:53 +0700

    feat(domain): add IHasDomainEvents interface for aggregate event tracking

---
+++ src/Domain/Common/IHasDomainEvents.cs
@@ -0,0 +1,9 @@
+using Domain.Events;
+
+namespace Domain.Common;
+
+public interface IHasDomainEvents
+{
+    IReadOnlyCollection<IDomainEvent> DomainEvents { get; }
+    void ClearDomainEvents();
+}
