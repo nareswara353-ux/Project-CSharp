7809f3084c5681ec1d7eb507fbd6c478dbd12901
Narezzzs <nareswara353@gmail.com>
2026-09-19 20:14:08 +0700

    feat(application): add reusable PaginationRequest DTO

---
+++ src/Application/Common/PaginationRequest.cs
@@ -0,0 +1,41 @@
+namespace Application.Common;
+
+public class PaginationRequest
+{
+    private const int MaxPageSize = 100;
+    private int _pageSize = 10;
+    private int _pageNumber = 1;
+
+    public int PageNumber
+    {
+        get => _pageNumber;
+        set => _pageNumber = value < 1 ? 1 : value;
+    }
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value < 1 ? 10 : (value > MaxPageSize ? MaxPageSize : value);
+    }
+
+    public int Skip => (PageNumber - 1) * PageSize;
+}
+
+public class PagedResult<T>
+{
+    public IReadOnlyList<T> Items { get; init; } = Array.Empty<T>();
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public int TotalCount { get; init; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    public bool HasPrevious => PageNumber > 1;
+    public bool HasNext => PageNumber < TotalPages;
+
+    public PagedResult(IReadOnlyList<T> items, int pageNumber, int pageSize, int totalCount)
+    {
+        Items = items;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+    }
+}
