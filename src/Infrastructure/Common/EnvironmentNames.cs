d20979529da1d3029aca0169487d2314f4fec6b0
Narezzzs <nareswara353@gmail.com>
2026-09-21 01:45:49 +0700

    feat(infrastructure): add EnvironmentNames constants

---
+++ src/Infrastructure/Common/EnvironmentNames.cs
@@ -0,0 +1,8 @@
+namespace Infrastructure.Common;
+
+public static class EnvironmentNames
+{
+    public const string Development = "Development";
+    public const string Staging = "Staging";
+    public const string Production = "Production";
+}
