b3fa6ea37344fe6878b70415ee64dc26c034ba1e
Narezzzs <nareswara353@gmail.com>
2026-09-21 01:45:05 +0700

    feat(webapi): add SwaggerTags constants for API grouping

---
+++ src/WebAPI/Common/SwaggerTags.cs
@@ -0,0 +1,11 @@
+namespace WebAPI.Common;
+
+public static class SwaggerTags
+{
+    public const string Authentication = "Authentication";
+    public const string Customers = "Customers";
+    public const string Products = "Products";
+    public const string Orders = "Orders";
+    public const string Users = "Users";
+    public const string Health = "Health";
+}
