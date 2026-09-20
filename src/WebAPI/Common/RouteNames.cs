950ca6528826ebff90e2995bf86838c6faaacc4b
Narezzzs <nareswara353@gmail.com>
2026-09-21 01:47:12 +0700

    feat(webapi): add RouteNames constants for action references

---
+++ src/WebAPI/Common/RouteNames.cs
@@ -0,0 +1,9 @@
+namespace WebAPI.Common;
+
+public static class RouteNames
+{
+    public const string GetCustomer = "GetCustomerById";
+    public const string GetProduct = "GetProductById";
+    public const string GetOrder = "GetOrderById";
+    public const string GetUser = "GetUserById";
+}
