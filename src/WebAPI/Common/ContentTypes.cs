6cde3f9c47a3716cf97819c0bc95270b6837068e
Narezzzs <nareswara353@gmail.com>
2026-09-21 01:45:00 +0700

    feat(webapi): add ContentTypes constants for MIME type usage

---
+++ src/WebAPI/Common/ContentTypes.cs
@@ -0,0 +1,8 @@
+namespace WebAPI.Common;
+
+public static class ContentTypes
+{
+    public const string ApplicationJson = "application/json";
+    public const string ProblemJson = "application/problem+json";
+    public const string TextPlain = "text/plain";
+}
