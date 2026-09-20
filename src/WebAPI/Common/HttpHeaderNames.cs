f5284d344e39d95c23234cf1f045ab02c44950e8
Narezzzs <nareswara353@gmail.com>
2026-09-21 01:43:35 +0700

    feat(webapi): add HttpHeaderNames constants for header usage

---
+++ src/WebAPI/Common/HttpHeaderNames.cs
@@ -0,0 +1,9 @@
+namespace WebAPI.Common;
+
+public static class HttpHeaderNames
+{
+    public const string CorrelationId = "X-Correlation-Id";
+    public const string RequestId = "X-Request-Id";
+    public const string ApiVersion = "X-Api-Version";
+    public const string Authorization = "Authorization";
+}
