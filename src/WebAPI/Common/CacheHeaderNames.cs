e0cbb792bdbaf8fd3a9791cb9a24454c1671dd21
Narezzzs <nareswara353@gmail.com>
2026-09-21 01:45:27 +0700

    feat(webapi): add CacheHeaderNames constants

---
+++ src/WebAPI/Common/CacheHeaderNames.cs
@@ -0,0 +1,10 @@
+namespace WebAPI.Common;
+
+public static class CacheHeaderNames
+{
+    public const string CacheControl = "Cache-Control";
+    public const string ETag = "ETag";
+    public const string IfNoneMatch = "If-None-Match";
+    public const string IfModifiedSince = "If-Modified-Since";
+    public const string LastModified = "Last-Modified";
+}
