48c50eba4c0e69110d6e6e78351f1225694a167b
Narezzzs <nareswara353@gmail.com>
2026-09-21 01:43:30 +0700

    feat(webapi): add Roles constants for authorization

---
+++ src/WebAPI/Common/Roles.cs
@@ -0,0 +1,10 @@
+namespace WebAPI.Common;
+
+public static class Roles
+{
+    public const string Admin = "Admin";
+    public const string Manager = "Manager";
+    public const string User = "User";
+
+    public const string AdminOrManager = Admin + "," + Manager;
+}
