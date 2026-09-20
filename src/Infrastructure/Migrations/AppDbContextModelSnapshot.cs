3a7ab852e261a15c4dda6c4afb6ea7dffcfe3da0
Narezzzs <nareswara353@gmail.com>
2026-09-20 16:55:01 +0700

    fix(infrastructure): convert model snapshot to file-scoped namespace

--- src/Infrastructure/Migrations/AppDbContextModelSnapshot.cs
+++ src/Infrastructure/Migrations/AppDbContextModelSnapshot.cs
@@ -6,169 +6,168 @@ using Infrastructure.Data;
 
 #nullable disable
 
-namespace Infrastructure.Migrations
+namespace Infrastructure.Migrations;
+
+[DbContext(typeof(AppDbContext))]
+partial class AppDbContextModelSnapshot : ModelSnapshot
 {
-    [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    protected override void BuildModel(ModelBuilder modelBuilder)
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.10")
-                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+        modelBuilder
+            .HasAnnotation("ProductVersion", "8.0.10")
+            .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
-            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
+        SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("Domain.Entities.Customer", b =>
-                {
-                    b.Property<Guid>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("uniqueidentifier")
-                        .HasColumnName("CustomerId");
+        modelBuilder.Entity("Domain.Entities.Customer", b =>
+            {
+                b.Property<Guid>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("uniqueidentifier")
+                    .HasColumnName("CustomerId");
 
-                    b.Property<DateTime>("CreatedAt")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("datetime2")
-                        .HasDefaultValueSql("GETUTCDATE()");
+                b.Property<DateTime>("CreatedAt")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("datetime2")
+                    .HasDefaultValueSql("GETUTCDATE()");
 
-                    b.Property<string>("FirstName")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+                b.Property<string>("FirstName")
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
 
-                    b.Property<bool>("IsActive")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("bit")
-                        .HasDefaultValue(true);
+                b.Property<bool>("IsActive")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("bit")
+                    .HasDefaultValue(true);
 
-                    b.Property<string>("LastName")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
+                b.Property<string>("LastName")
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnType("nvarchar(50)");
 
-                    b.Property<DateTime?>("UpdatedAt")
-                        .HasColumnType("datetime2");
+                b.Property<DateTime?>("UpdatedAt")
+                    .HasColumnType("datetime2");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("Customers");
-                });
+                b.ToTable("Customers");
+            });
 
-            modelBuilder.Entity("Domain.Entities.Customer", b =>
-                {
-                    b.OwnsOne("Domain.ValueObjects.Email", "Email", b1 =>
-                        {
-                            b1.Property<Guid>("CustomerId")
-                                .HasColumnType("uniqueidentifier");
+        modelBuilder.Entity("Domain.Entities.Customer", b =>
+            {
+                b.OwnsOne("Domain.ValueObjects.Email", "Email", b1 =>
+                    {
+                        b1.Property<Guid>("CustomerId")
+                            .HasColumnType("uniqueidentifier");
 
-                            b1.Property<string>("Value")
-                                .IsRequired()
-                                .HasMaxLength(255)
-                                .HasColumnType("nvarchar(255)")
-                                .HasColumnName("Email");
+                        b1.Property<string>("Value")
+                            .IsRequired()
+                            .HasMaxLength(255)
+                            .HasColumnType("nvarchar(255)")
+                            .HasColumnName("Email");
 
-                            b1.HasKey("CustomerId");
+                        b1.HasKey("CustomerId");
 
-                            b1.HasIndex("Value")
-                                .IsUnique();
+                        b1.HasIndex("Value")
+                            .IsUnique();
 
-                            b1.ToTable("Customers");
+                        b1.ToTable("Customers");
 
-                            b1.WithOwner()
-                                .HasForeignKey("CustomerId");
-                        });
+                        b1.WithOwner()
+                            .HasForeignKey("CustomerId");
+                    });
 
-                    b.OwnsOne("Domain.ValueObjects.Address", "BillingAddress", b1 =>
-                        {
-                            b1.Property<Guid>("CustomerId")
-                                .HasColumnType("uniqueidentifier");
+                b.OwnsOne("Domain.ValueObjects.Address", "BillingAddress", b1 =>
+                    {
+                        b1.Property<Guid>("CustomerId")
+                            .HasColumnType("uniqueidentifier");
 
-                            b1.Property<string>("City")
-                                .IsRequired()
-                                .HasMaxLength(100)
-                                .HasColumnType("nvarchar(100)")
-                                .HasColumnName("BillingCity");
+                        b1.Property<string>("City")
+                            .IsRequired()
+                            .HasMaxLength(100)
+                            .HasColumnType("nvarchar(100)")
+                            .HasColumnName("BillingCity");
 
-                            b1.Property<string>("Country")
-                                .IsRequired()
-                                .HasMaxLength(100)
-                                .HasColumnType("nvarchar(100)")
-                                .HasColumnName("BillingCountry");
+                        b1.Property<string>("Country")
+                            .IsRequired()
+                            .HasMaxLength(100)
+                            .HasColumnType("nvarchar(100)")
+                            .HasColumnName("BillingCountry");
 
-                            b1.Property<string>("PostalCode")
-                                .IsRequired()
-                                .HasMaxLength(20)
-                                .HasColumnType("nvarchar(20)")
-                                .HasColumnName("BillingPostalCode");
+                        b1.Property<string>("PostalCode")
+                            .IsRequired()
+                            .HasMaxLength(20)
+                            .HasColumnType("nvarchar(20)")
+                            .HasColumnName("BillingPostalCode");
 
-                            b1.Property<string>("State")
-                                .IsRequired()
-                                .HasMaxLength(100)
-                                .HasColumnType("nvarchar(100)")
-                                .HasColumnName("BillingState");
+                        b1.Property<string>("State")
+                            .IsRequired()
+                            .HasMaxLength(100)
+                            .HasColumnType("nvarchar(100)")
+                            .HasColumnName("BillingState");
 
-                            b1.Property<string>("Street")
-                                .IsRequired()
-                                .HasMaxLength(200)
-                                .HasColumnType("nvarchar(200)")
-                                .HasColumnName("BillingStreet");
+                        b1.Property<string>("Street")
+                            .IsRequired()
+                            .HasMaxLength(200)
+                            .HasColumnType("nvarchar(200)")
+                            .HasColumnName("BillingStreet");
 
-                            b1.HasKey("CustomerId");
+                        b1.HasKey("CustomerId");
 
-                            b1.ToTable("Customers");
+                        b1.ToTable("Customers");
 
-                            b1.WithOwner()
-                                .HasForeignKey("CustomerId");
-                        });
+                        b1.WithOwner()
+                            .HasForeignKey("CustomerId");
+                    });
 
-                    b.OwnsOne("Domain.ValueObjects.Address", "ShippingAddress", b1 =>
-                        {
-                            b1.Property<Guid>("CustomerId")
-                                .HasColumnType("uniqueidentifier");
+                b.OwnsOne("Domain.ValueObjects.Address", "ShippingAddress", b1 =>
+                    {
+                        b1.Property<Guid>("CustomerId")
+                            .HasColumnType("uniqueidentifier");
 
-                            b1.Property<string>("City")
-                                .HasMaxLength(100)
-                                .HasColumnType("nvarchar(100)")
-                                .HasColumnName("ShippingCity");
+                        b1.Property<string>("City")
+                            .HasMaxLength(100)
+                            .HasColumnType("nvarchar(100)")
+                            .HasColumnName("ShippingCity");
 
-                            b1.Property<string>("Country")
-                                .HasMaxLength(100)
-                                .HasColumnType("nvarchar(100)")
-                                .HasColumnName("ShippingCountry");
+                        b1.Property<string>("Country")
+                            .HasMaxLength(100)
+                            .HasColumnType("nvarchar(100)")
+                            .HasColumnName("ShippingCountry");
 
-                            b1.Property<string>("PostalCode")
-                                .HasMaxLength(20)
-                                .HasColumnType("nvarchar(20)")
-                                .HasColumnName("ShippingPostalCode");
+                        b1.Property<string>("PostalCode")
+                            .HasMaxLength(20)
+                            .HasColumnType("nvarchar(20)")
+                            .HasColumnName("ShippingPostalCode");
 
-                            b1.Property<string>("State")
-                                .HasMaxLength(100)
-                                .HasColumnType("nvarchar(100)")
-                                .HasColumnName("ShippingState");
+                        b1.Property<string>("State")
+                            .HasMaxLength(100)
+                            .HasColumnType("nvarchar(100)")
+                            .HasColumnName("ShippingState");
 
-                            b1.Property<string>("Street")
-                                .HasMaxLength(200)
-                                .HasColumnType("nvarchar(200)")
-                                .HasColumnName("ShippingStreet");
+                        b1.Property<string>("Street")
+                            .HasMaxLength(200)
+                            .HasColumnType("nvarchar(200)")
+                            .HasColumnName("ShippingStreet");
 
-                            b1.HasKey("CustomerId");
+                        b1.HasKey("CustomerId");
 
-                            b1.ToTable("Customers");
+                        b1.ToTable("Customers");
 
-                            b1.WithOwner()
-                                .HasForeignKey("CustomerId");
-                        });
+                        b1.WithOwner()
+                            .HasForeignKey("CustomerId");
+                    });
 
-                    b.Navigation("BillingAddress")
-                        .IsRequired();
+                b.Navigation("BillingAddress")
+                    .IsRequired();
 
-                    b.Navigation("Email")
-                        .IsRequired();
+                b.Navigation("Email")
+                    .IsRequired();
 
-                    b.Navigation("ShippingAddress");
-                });
+                b.Navigation("ShippingAddress");
+            });
 #pragma warning restore 612, 618
-        }
     }
 }
