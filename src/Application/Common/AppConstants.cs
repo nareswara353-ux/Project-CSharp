6991315840803ef8d6ed54d7235e485f2b575e59
Narezzzs <nareswara353@gmail.com>
2026-09-21 01:46:39 +0700

    feat(application): add AppConstants for shared configuration values

---
+++ src/Application/Common/AppConstants.cs
@@ -0,0 +1,17 @@
+namespace Application.Common;
+
+public static class AppConstants
+{
+    public const int DefaultPageNumber = 1;
+    public const int DefaultPageSize = 10;
+    public const int MaxPageSize = 100;
+    public const int MinPasswordLength = 8;
+    public const int MaxUsernameLength = 50;
+    public const int MinUsernameLength = 3;
+    public const int MaxNameLength = 50;
+    public const int MaxDescriptionLength = 2000;
+    public const int MaxNotesLength = 1000;
+    public const int PerformanceThresholdMs = 500;
+    public const int MaxRetryCount = 5;
+    public const int CommandTimeoutSeconds = 60;
+}
