b7f1221131ac35cd85a720d995ba56fdf33e1ed3
Narezzzs <nareswara353@gmail.com>
2026-09-21 01:46:48 +0700

    feat(application): add EmailSubjects constants

---
+++ src/Application/Common/EmailSubjects.cs
@@ -0,0 +1,13 @@
+namespace Application.Common;
+
+public static class EmailSubjects
+{
+    public const string WelcomeCustomer = "Welcome to Portfolio Enterprise";
+    public const string CustomerDeactivated = "Your account has been deactivated";
+    public const string OrderConfirmation = "Order Confirmation";
+    public const string OrderShipped = "Your order has been shipped";
+    public const string OrderCancelled = "Order Cancellation Notice";
+    public const string UserWelcome = "Welcome! Please verify your email";
+    public const string PasswordChanged = "Your password has been changed";
+    public const string StockLowAlert = "Stock Low Alert";
+}
