0033e8f56c438e5913307b3e3c46a7c17b865c11
Narezzzs <nareswara353@gmail.com>
2026-09-21 01:45:44 +0700

    feat(webapi): add PolicyNames constants for authorization

---
+++ src/WebAPI/Common/PolicyNames.cs
@@ -0,0 +1,8 @@
+namespace WebAPI.Common;
+
+public static class PolicyNames
+{
+    public const string AdminOnly = "AdminOnly";
+    public const string ManagerOrAbove = "ManagerOrAbove";
+    public const string AuthenticatedUser = "AuthenticatedUser";
+}
