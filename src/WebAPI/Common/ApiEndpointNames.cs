024e9bd682da22e4dabb2ff0ff813a66f8a5ca04
Narezzzs <nareswara353@gmail.com>
2026-09-21 01:42:58 +0700

    feat(webapi): add ApiEndpointNames constants for action references

---
+++ src/WebAPI/Common/ApiEndpointNames.cs
@@ -0,0 +1,9 @@
+namespace WebAPI.Common;
+
+public static class ApiEndpointNames
+{
+    public const string GetCustomerById = "GetById";
+    public const string GetProductById = "GetById";
+    public const string GetOrderById = "GetById";
+    public const string GetUserById = "GetById";
+}
