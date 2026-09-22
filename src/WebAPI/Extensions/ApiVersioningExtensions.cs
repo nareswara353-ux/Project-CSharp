e4ec3c90a781e29d6ddd51736efe51d7ad33d4c6
Narezzzs <nareswara353@gmail.com>
2026-09-23 05:50:05 +0700

    feat(webapi): add ApiVersioningExtensions for versioned endpoints

---
+++ src/WebAPI/Extensions/ApiVersioningExtensions.cs
@@ -0,0 +1,25 @@
+using Asp.Versioning;
+
+namespace WebAPI.Extensions;
+
+public static class ApiVersioningExtensions
+{
+    public static IServiceCollection AddApiVersioningSupport(this IServiceCollection services)
+    {
+        services
+            .AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+                options.ApiVersionReader = new UrlSegmentApiVersionReader();
+            })
+            .AddApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+        return services;
+    }
+}
