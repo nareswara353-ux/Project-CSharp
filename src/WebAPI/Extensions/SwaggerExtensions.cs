32b71727041012f763dfb94698024942c73c5fde
Narezzzs <nareswara353@gmail.com>
2026-09-23 05:51:56 +0700

    feat(webapi): support multi-version Swagger documentation

---
+++ src/WebAPI/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,60 @@
+using Asp.Versioning.ApiExplorer;
+using Microsoft.OpenApi.Models;
+
+namespace WebAPI.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Enter JWT token (without 'Bearer ' prefix)."
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseVersionedSwagger(
+        this IApplicationBuilder app,
+        IApiVersionDescriptionProvider provider)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                options.SwaggerEndpoint(
+                    $"/swagger/{description.GroupName}/swagger.json",
+                    $"Portfolio Enterprise API {description.GroupName.ToUpperInvariant()}");
+            }
+
+            options.RoutePrefix = "swagger";
+        });
+
+        return app;
+    }
+}
