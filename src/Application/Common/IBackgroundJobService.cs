a358ff01770cc7f2aac99951eefeef5e66651c9a
Narezzzs <nareswara353@gmail.com>
2026-09-22 22:55:50 +0700

    fix(application): add notnull constraint on IBackgroundJobService generics

--- src/Application/Common/IBackgroundJobService.cs
+++ src/Application/Common/IBackgroundJobService.cs
@@ -4,14 +4,14 @@ namespace Application.Common;
 
 public interface IBackgroundJobService
 {
-    void Enqueue<T>(Expression<Func<T, Task>> methodCall);
+    void Enqueue<T>(Expression<Func<T, Task>> methodCall) where T : notnull;
 
-    void Schedule<T>(Expression<Func<T, Task>> methodCall, TimeSpan delay);
+    void Schedule<T>(Expression<Func<T, Task>> methodCall, TimeSpan delay) where T : notnull;
 
     void AddOrUpdateRecurring<T>(
         string jobId,
         Expression<Func<T, Task>> methodCall,
-        string cronExpression);
+        string cronExpression) where T : notnull;
 }
 
 public static class JobIds
