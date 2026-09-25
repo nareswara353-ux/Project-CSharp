8a0d1a6e6070e789dd35bf131f7bac6f80f0a011
Narezzzs <nareswara353@gmail.com>
2026-09-25 20:26:52 +0700

    feat(webapi): add HasPermissionAttribute for permission-based authorization

---
+++ src/WebAPI/Attributes/HasPermissionAttribute.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebAPI.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public sealed class HasPermissionAttribute : AuthorizeAttribute
+{
+    public const string PolicyPrefix = "Permission:";
+
+    public HasPermissionAttribute(string permission)
+    {
+        Policy = $"{PolicyPrefix}{permission}";
+    }
+}
